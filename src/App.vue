a7b01f92df80ddd6711e45c837094d71ab2534f8
Gino-Y <ginoyan250@gmail.com>
2024-03-01 22:15:07 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -55,6 +55,26 @@ import ZWJS from "@/components/ZWJS.vue";
 </template>
 
 <style scoped>
+*{
+  user-select: none;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  ms-user-select: none;
+}
+*{
+  -webkit-touch-callout:none;  /*系统默认菜单被禁用*/
+  -webkit-user-select:none; /*webkit浏览器*/
+  -khtml-user-select:none; /*早期浏览器*/
+  -moz-user-select:none;/*火狐*/
+  -ms-user-select:none; /*IE10*/
+  user-select:none;
+}
+input,textarea {
+  -webkit-user-select:auto; /*webkit浏览器*/
+  margin: 0px;
+  padding: 0px;
+  outline: none;
+}
 h3{
   color: white;
   margin-left: 10px;
