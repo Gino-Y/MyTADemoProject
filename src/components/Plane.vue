5824bfc3bf4347715c558f75a187f9c0da83da70
Gino-Y <ginoyan250@gmail.com>
2024-03-01 18:41:00 +0800

    123

--- src/components/Plane.vue
+++ src/components/Plane.vue
@@ -3,14 +3,19 @@
   <div class="test3">
     <div class="in">
       <div class="in">  <img
+          loading="lazy"
           src="../assets/故障效果-1.gif"
           alt="展示的图片"
           width=100%
           height=100%
       >故障效果</div>
     </div>
+  </div>
+
+  <div class="test3">
     <div class="in">
       <div class="in">  <img
+          loading="lazy"
           src="../assets/故障效果-2.gif"
           alt="展示的图片"
           width=100%
@@ -22,25 +27,34 @@
   <div class="test3">
     <div class="in">
       <div class="in">  <img
+          loading="lazy"
           src="../assets/角色卡通渲染.gif"
           alt="展示的图片"
-          width=70%
-          height=70%
+          width=100%
+          height=100%
       ><br>角色卡通渲染</div>
     </div>
     <div class="in">
       <div class="in">  <img
+          loading="lazy"
           src="../assets/星座模拟.gif"
           alt="展示的图片"
           width=100%
           height=100%
-      >星座模拟</div>
+      ><br>星座模拟<br><img
+          loading="lazy"
+          src="../assets/能量网格流光.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      ><br>能量网格流光</div>
     </div>
   </div>
 
   <div class="test3">
     <div class="in">
       <div class="in">  <img
+          loading="lazy"
           src="../assets/低成本角色材质.gif"
           alt="展示的图片"
           width=100%
@@ -48,6 +62,225 @@
       >低成本角色材质</div>
     </div>
   </div>
+
+  <div class="test3">
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/溶解效果-4.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >溶解效果</div>
+    </div>
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/溶解效果-2.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >溶解效果</div>
+    </div>
+  </div>
+
+  <div class="test3">
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/溶解效果-1.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >溶解效果</div>
+    </div>
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/溶解效果-3.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >溶解效果</div>
+    </div>
+  </div>
+
+  <div class="test3">
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/2D项目3D光影效果制作-2.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >2D项目3D光影实现</div>
+    </div>
+  </div>
+
+  <div class="test3">
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/2D项目3D光影效果制作-1.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >2D项目3D光影实现</div>
+    </div>
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/2D项目3D光影效果制作-3.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >2D项目3D光影实现</div>
+    </div>
+  </div>
+
+  <div class="test3">
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/视差.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >视差</div>
+    </div>
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/全息虚拟沙盘.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >全息虚拟沙盘</div>
+    </div>
+  </div>
+
+  <div class="test3">
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/PBR.jpg"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >PBR渲染</div>
+    </div>
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/武器变形.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >武器变形
+        <img
+            loading="lazy"
+            src="../assets/道具3D淡入淡出效果.gif"
+            alt="展示的图片"
+            width=100%
+            height=100%
+        >
+        道具3D淡入淡出效果</div>
+    </div>
+  </div>
+
+  <div class="test3">
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/FlowMap-1.gif"
+          alt="展示的图片"
+          width=100%
+          height=150px
+      >FlowMap</div>
+    </div>
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/FlowMap-2.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >FlowMap</div>
+    </div>
+  </div>
+
+  <div class="test3">
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/动态灯光雾-1.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >动态灯光雾</div>
+    </div>
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/动态灯光雾-2.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >动态灯光雾</div>
+    </div>
+  </div>
+
+  <div class="test3">
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/交互液体.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >3D交互液体</div>
+    </div>
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/2D液体实现-1.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >2D液体实现</div>
+    </div>
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/2D液体实现-2.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >2D液体实现</div>
+    </div>
+  </div>
+
+  <div class="test3">
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/水体.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >动态灯光雾</div>
+    </div>
+    <div class="in">
+      <div class="in">  <img
+          loading="lazy"
+          src="../assets/冰雪材质.gif"
+          alt="展示的图片"
+          width=100%
+          height=100%
+      >动态灯光雾</div>
+    </div>
+  </div>
+
 
 </template>
 
@@ -62,11 +295,11 @@
   /*height: 50px;*/
   /*padding: 10px;*/
   margin: 5px;
-  background-color: rgba(0, 0, 0, 0.39);
+  background-color: rgba(0, 0, 0, 0.44);
   border-style: none;
   /*border-color: #803939;*/
   box-sizing: border-box;
   text-align:center;
-  color: #acafb2;
+  color: #67696b;
 }
 </style>
