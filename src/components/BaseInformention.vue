8847873943bc519d724323bd55ebd36a086617f4
Gino-Y <ginoyan250@gmail.com>
2024-03-01 21:49:18 +0800

    123

--- src/components/BaseInformention.vue
+++ src/components/BaseInformention.vue
@@ -1,9 +1,9 @@
 <template>
   <el-table :data="tableData" style="width: 100%">
-    <el-table-column prop="date" label="姓名" width="80" />
+    <el-table-column prop="date" label="姓名" width="70" />
     <el-table-column prop="name" label="性别" width="60" />
-    <el-table-column prop="address" label="出生日期" width="120"/>
-    <el-table-column prop="phone" label="手机/微信" width="120"/>
+    <el-table-column prop="address" label="出生日期" width="100"/>
+    <el-table-column prop="phone" label="手机/微信" width="130"/>
   </el-table>
 </template>
 
