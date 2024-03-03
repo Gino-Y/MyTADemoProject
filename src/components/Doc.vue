08d3892330fb65cb36a1d438cf5d34fda8986994
Gino-Y <ginoyan250@gmail.com>
2024-03-03 19:31:49 +0800

    123

--- src/components/Doc.vue
+++ src/components/Doc.vue
@@ -1,15 +1,15 @@
 <template>
   <el-timeline>
     <el-timeline-item timestamp="2018/12—2023/10" placement="top">
-      <el-card>
-        <h3>杭州聆曦网络科技有限公司（原 杭州藤木网络科技）</h3>
+      <el-card style="width: 96%">
+        <h3>杭州聆曦网络科技有限公司（原 杭州藤木网络科技</h3>
         <p>职位：技术美术</p>
         <p>职责: 根据美术及引擎要求开发定制化shader，保证效果的前提控制编译指令效率。技术文档撰写。</p>
         <p>项目：《阿拉德之怒》、类DNF2.0项目、A8项目等 </p>
       </el-card>
     </el-timeline-item>
     <el-timeline-item timestamp="2018/05—2018/11 " placement="top">
-      <el-card>
+      <el-card style="width: 96%">
         <h3>上海指舞网络科技</h3>
         <p>职位：技术美术 </p>
         <p>职责: 美术风格及质量把控<br>资源输出及整合 </p>
@@ -17,7 +17,7 @@
       </el-card>
     </el-timeline-item>
     <el-timeline-item timestamp="2017/12—2018/05" placement="top">
-      <el-card>
+      <el-card style="width: 96%">
         <h3>维塔士电脑软件有限公司</h3>
         <p>职位：技术美术 </p>
         <p>职责: 配合引擎（Anvil）研发团队，提供效果，性能及实用性反馈。       为美术团队定制工作流程及标准化技术文档。       解决图形技术问题</p>
@@ -25,7 +25,7 @@
       </el-card>
     </el-timeline-item>
     <el-timeline-item timestamp="2014/09—2017/12	" placement="top">
-      <el-card>
+      <el-card style="width: 96%">
         <h3>乐卓网络科技（上海）股份有限公司</h3>
         <p>职位：技术美术 </p>
         <p>职责: 优化画面效果及图形效率。       提供图形技术支持       美术团队引擎及软件培训</p>
@@ -33,7 +33,7 @@
       </el-card>
     </el-timeline-item>
     <el-timeline-item timestamp="2012/11—2014/08" placement="top">
-      <el-card>
+      <el-card style="width: 96%">
         <h3>上海骏梦科技有限公司</h3>
         <p>职位：模型师/地编 </p>
         <p>职责: 模型贴图制作，关卡场景制作</p>
@@ -41,7 +41,7 @@
       </el-card>
     </el-timeline-item>
     <el-timeline-item timestamp="2010/06—2012/10" placement="top">
-      <el-card>
+      <el-card style="width: 96%">
         <h3>上海明器多媒体科技有限公司</h3>
         <p>职位：模型贴图灯光渲染 </p>
         <p>职责: 地图制作，灯光气氛制作及后期渲染。</p>
--- src/components/Doc2.vue
+++ src/components/Doc2.vue
@@ -1,7 +1,7 @@
 <template>
   <el-timeline>
     <el-timeline-item timestamp="2007/09—2010/09" placement="top">
-      <el-card>
+      <el-card style="width: 96%">
         <h3>吉林艺术学院</h3>
         <p>专业：影视动画</p>
       </el-card>
